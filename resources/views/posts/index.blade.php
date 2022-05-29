aea44100a9b4705df5fe1688d6787679d26d5b53
Aykhan Amiraslanli <ayxano@gmail.com>
2022-05-29 23:23:12 +0400

    changed content for more informal

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,7 @@
     <div><h2>Testing for loop</h2></div>
     
     @for ($i = 0; $i <= 10; $i++)
-        <div>The current value is {{ $i }}</div>
+        <div>The current value inside for loop is {{ $i }}</div>
     @endfor
 
     <div><h2>Testing while</h2></div>
