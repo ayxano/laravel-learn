b0b4bb755b01bf00cd378ce3b1c8f790e5910a49
Aykhan Amiraslanli <ayxano@gmail.com>
2022-05-29 22:57:39 +0400

    posts.show view restyled

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,24 +3,24 @@
 @section('title', $post['title'])
 
 @section('content')
-@if ($post['is_new'])
-    <div>Blog post is new using if</div>
-@else
-    <div>Blog post is old using if</div>
-@endif
+    @if ($post['is_new'])
+        <div>Blog post is new using if</div>
+    @else
+        <div>Blog post is old using if</div>
+    @endif
 
 
-@unless ($post['is_new'])
-    <div>It is an old post using unless</div>
-@endunless
+    @unless ($post['is_new'])
+        <div>It is an old post using unless</div>
+    @endunless
 
 
-<h1>{{ $post['title'] }}</h1>
-<p>{{ $post['content'] }}</p>
+    <h1>{{ $post['title'] }}</h1>
+    <p>{{ $post['content'] }}</p>
 
-@isset($post['has_comments'])
-    <div>This post has some comments... using isset</div>
-@endisset
+    @isset($post['has_comments'])
+        <div>This post has some comments... using isset</div>
+    @endisset
 
 
 @endsection
