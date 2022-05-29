20d61341715b20cb457819db89b65bd8bf629e98
Aykhan Amiraslanli <ayxano@gmail.com>
2022-05-29 23:03:18 +0400

    More informal changes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,10 +6,10 @@
     @if ($post['is_new'])
         <div>Blog post is new using if</div>
     @else
-        <div>Blog post is old using if</div>
+        <div>Blog post is old using elseif/else</div>
     @endif
 
-
+    {{-- if input condition is false, then it will be rendered  --}}
     @unless ($post['is_new'])
         <div>It is an old post using unless</div>
     @endunless
