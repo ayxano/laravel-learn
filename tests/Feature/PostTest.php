a87e32f8c86fe1fee07c96b70f36a95325c288b3
Aykhan Amiraslanli <ayxano@gmail.com>
2022-07-25 14:06:28 +0400

    Add post by user form test

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\BlogPost;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
 class PostTest extends TestCase
@@ -34,5 +35,22 @@ class PostTest extends TestCase
         $this->assertDatabaseHas('blog_posts', [
             'title' => 'New phpunit blog post'
         ]);
+    }
+
+    // Simulating post request to save data on db
+    public function testStoreValid() :void 
+    {
+        // Starting session for CSRF token
+        Session::start();
+        $params = [
+            'title' => 'Valid title',
+            'content' => 'At least 10 characters',
+            '_token' => csrf_token()
+        ];
+        $this->post('/posts', $params) // sending data to this endpoint
+        ->assertStatus(302) // waiting response status code for 302
+        ->assertSessionHas('status'); // also check session has 'status' key
+
+        $this->assertEquals(session('status'), 'Created on DB!'); // check 'status' key on session is equal to 'Created on DB!'
     }
 }
