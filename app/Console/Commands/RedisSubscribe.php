940dc47b2a396037848591a82b99a1e1c0fefe94
Aykhan Amiraslanli <ayxano@gmail.com>
2022-10-23 23:22:16 +0400

    Added wildcard channel examples for Redis Subscribe

--- app/Console/Commands/RedisSubscribe.php
+++ app/Console/Commands/RedisSubscribe.php
@@ -39,8 +39,21 @@ class RedisSubscribe extends Command
     public function handle() // Receiver. For example: This is radio listener
     {
         ini_set('default_socket_timeout', -1); // add this to prevent connection timeouts
-        Redis::subscribe(['test-channel'], function ($message) {
-            echo $message . PHP_EOL;
+        Redis::subscribe(['test-channel'], function ($message, $channel) {
+            echo 'Channel: ' . $channel . PHP_EOL;
+            echo 'Message: ' . $message . PHP_EOL . PHP_EOL;
         });
+
+        /* OR use Wildcard Subscriptions 
+        Redis::psubscribe(['*'], function ($message, $channel) {
+            echo 'Channel: ' . $channel . PHP_EOL;
+            echo 'Message: ' . $message . PHP_EOL . PHP_EOL;
+        });
+        
+        Redis::psubscribe(['users.*'], function ($message, $channel) {
+            echo 'Channel: ' . $channel . PHP_EOL;
+            echo 'Message: ' . $message . PHP_EOL . PHP_EOL;
+        });
+        */
     }
 }
