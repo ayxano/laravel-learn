6f6edb31247c774d63cd511e1a63ba5abeacc113
Aykhan Amiraslanli <ayxano@gmail.com>
2022-10-25 03:10:21 +0400

    QUEUE INTRODUCTION:  Convert mail sending to queue

--- app/Mail/PostAdded.php
+++ app/Mail/PostAdded.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class PostAdded extends Mailable
+class PostAdded extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
