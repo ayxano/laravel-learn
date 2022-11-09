8d90fd927b194c588e5672c0e969b56d1d316601
ayxano <ayxano@gmail.com>
2022-11-10 01:43:58 +0400

    hide value on models when serializing

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class BlogPost extends Model
 {
     protected $fillable =   ['title', 'content'];
+    protected $hidden   =   ['updated_at'];
     use HasFactory;
 }
